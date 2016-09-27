725f8901dc5c6f3a3ddebfabaea9cd0d3b78e173
theswade <samwade1492@gmail.com>
2016-09-27 13:21:48 -0600

    i changed numbers

--- ThreadExample/src/Example/Example.java
+++ ThreadExample/src/Example/Example.java
@@ -3,7 +3,7 @@ class ThrdExample implements Runnable {
 
     @Override
     public void run() {
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < 9; i++) {
             System.out.println(Thread.currentThread().getId() +" number " + i);
         }
 
@@ -13,7 +13,7 @@ class ThrdExample implements Runnable {
 public class Example {
     public static void main(String args[]) {
 
-        for(int i = 1; i < 5; i++){
+        for(int i = 1; i < 4; i++){
             Thread ThrdExample = new Thread (new ThrdExample());
             ThrdExample.start();
             Thread ThrdExample1 = new Thread (new ThrdExample());
