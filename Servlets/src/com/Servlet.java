2e20396722157dcb64a12e6253466b9c1e0092a6
CarleeOuwerkerk <carleeouwerkerk@shaw.ca>
2016-10-25 20:49:19 -0600

    1

--- Servlets/src/com/Servlet.java
+++ Servlets/src/com/Servlet.java
@@ -1,19 +1,18 @@
 package com;
 
+import org.quickconnectfamily.json.JSONInputStream;
+import org.quickconnectfamily.json.JSONOutputStream;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
-
-//import org.quickconnectfamily.json.JSONException;
-//import org.quickconnectfamily.json.JSONInputStream;
-//import org.quickconnectfamily.json.JSONOutputStream;
-//import org.quickconnectfamily.json.JSONUtilities;
-
-//import org.json.JSONObject;
 
 
 /**
@@ -24,32 +23,32 @@ public class Servlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//        HashMap map = new HashMap();
-//
-//        map.put("Canada", 2);
-//        map.put("USA", 8);
-//        map.put("Mexico", 1);
-//
-//
-////        JSONObject jsonObject = new JSONObject(map);
-////
-//
-//        File fileOne = new File("fileOne.txt");
-//
-//        FileInputStream fileIn = new FileInputStream(fileOne);
-//        FileOutputStream fileOut = new FileOutputStream(fileOne);
-//
-////        JSONInputStream inFromClient = new JSONInputStream(fileIn);
-////        JSONOutputStream outToClient = new JSONOutputStream(FileOut);
+        HashMap map = new HashMap();
+
+        map.put("Canada", "2");
+        map.put("USA", "8");
+        map.put("Mexico", "1");
+
+
+//        JSONObject jsonObject = new JSONObject(map);
+
+
+        File fileOne = new File("fileOne.txt");
+
+        FileInputStream fileIn = new FileInputStream(fileOne);
+        FileOutputStream fileOut = new FileOutputStream(fileOne);
+
+        JSONInputStream inFromClient = new JSONInputStream(fileIn);
+        JSONOutputStream outToClient = new JSONOutputStream(fileOut);
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Test</h1>");
-        out.flush();
-//
+
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        out.println("<h1>Test</h1>");
+//        out.flush();
+
     }
 }
